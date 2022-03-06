bcc40b2038b5e9c7804d0fbae086e4c46ef6ac2b
Unknownbruh893 <101063856+Unknownbruh893@users.noreply.github.com>
2022-03-06 19:49:49 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,1 @@
-type Script.lua > Ironbrew/netcoreapp2.0/input.txt
-cd Ironbrew/netcoreapp2.0/
-dotnet "IronBrew2 CLI.dll" "input.txt"
-cd ../..
-luac Ironbrew/netcoreapp2.0/out.lua
-lua Main.lua
-pause
+print("unkniwn") 
